8a35570fc8598076df92fcf6abb7cb0ef2a0cc40
zaid2 <zaid.tifaf101@gmail.com>
2023-12-06 20:16:16 +0100

    5

--- api/function_app.py
+++ api/function_app.py
@@ -2,9 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/api')
-def api():
-    return 'Hi, I\'m the API!'
+@app.route('/')
+def index():
+    return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
