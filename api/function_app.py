41681e837b8f6c3fb63cf04703e741a1021f5e00
zaid2 <zaid.tifaf101@gmail.com>
2023-12-09 17:19:27 +0100

    hi

--- api/function_app.py
+++ api/function_app.py
@@ -1,9 +1,7 @@
 import logging
-from flask import app
 import azure.functions as func
 
-@app.route(route="azureproject", auth_level=func.AuthLevel.undefined)
-def azureproject(req: func.HttpRequest) -> func.HttpResponse:
+def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
     name = req.params.get('name')
